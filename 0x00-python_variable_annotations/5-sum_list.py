73a9d7de1849eaa32ea4be48e712404769f48593
JSM788 <salinasjuan788@gmail.com>
2022-12-08 22:41:39 -0500

    Adding 5-sum_list.py file

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Defining the function to_str"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """Returns the string representation of the float"""
+    result = sum(input_list)
+    return float(result)
