779f11c5c93ef2be5c860287f0092194a463f433
JSM788 <salinasjuan788@gmail.com>
2022-12-12 12:53:54 -0500

    Adding 0-basic_async_syntax.py file

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" This asynchronous coroutine that takes in an integer argument named
+wait_random that waits for a random delay between 0 and max_delay seconds"""
+import asyncio
+import random
+
+
+async def wait_random(max_delay=10):
+    """This function returns a delay"""
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
