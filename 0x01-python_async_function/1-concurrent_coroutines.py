59c60d31a4f0529a01c9815baf921e857699088c
JSM788 <salinasjuan788@gmail.com>
2022-12-12 22:04:37 -0500

    Creating a task

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -2,6 +2,7 @@
 """Write an async routine called wait_n that takes in 2 int arguments
 n and max_delay"""
 from typing import List
+import asyncio
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
@@ -10,7 +11,7 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     """This function return the list of all the delays in order"""
     array = []
     for i in range(n):
-        delay = await wait_random(max_delay)
+        delay = await asyncio.create_task(wait_random(max_delay))
         array.append(delay)
     result = sorted(array)
     return result
