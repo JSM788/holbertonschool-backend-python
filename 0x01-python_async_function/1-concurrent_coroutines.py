9dfec0b5a6b93aeee68eac9e61a0800abce5ad2c
JSM788 <salinasjuan788@gmail.com>
2022-12-12 20:23:25 -0500

    Adding type

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """Write an async routine called wait_n that takes in 2 int arguments
 n and max_delay"""
+from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n, max_delay):
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """This function return the list of all the delays in order"""
     array = []
     for i in range(n):
