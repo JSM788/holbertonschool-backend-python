2c78dab472a5f04beff82139e39a0b7b32209f46
JSM788 <salinasjuan788@gmail.com>
2022-12-13 20:16:06 -0500

    Adding type

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,9 +2,10 @@
 """Defining a coroutine that takes no arguments"""
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """This coroutine returns random number between 0 and 10"""
     for i in range(10):
         await asyncio.sleep(1)
