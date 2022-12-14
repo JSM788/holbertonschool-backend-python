1403a7980fbbbfeae97f23945e6ea374c1fd4884
JSM788 <salinasjuan788@gmail.com>
2022-12-13 22:34:10 -0500

    Addign Generator type

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -6,6 +6,6 @@ import asyncio
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> Generator[float, None, None]:
     """This method returns 10 random numbers"""
     return [i async for i in async_generator()]
